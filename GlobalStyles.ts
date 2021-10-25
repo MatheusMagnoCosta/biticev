212bac2a09602bb06c4e38b15a97d8ab7169c87c
Lia Mariana <marianabrito1791@gmail.com>
2021-10-24 21:07:01 -0300

    Responsividade

---
+++ GlobalStyles.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    * {
+        margin:0;
+        padding:0;
+        box-sizing:border-box;
+        scroll-behavior: smooth;
+    }
+    *, input, button{
+        font-family: 'Roboto', sans-serif;
+    }
+    body{
+        background-color: #1A1824;
+    }
+`;
